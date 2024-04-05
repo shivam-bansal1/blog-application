47298b4627fb48b4011590f4e8d790cc054252b8
Shivam Bansal <shivambnsal@gmail.com>
2024-04-06 01:27:14 +0530

    template inheritance and custom context_processors

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,12 +3,10 @@ from django.shortcuts import render
 from blogs.models import Category, Blog
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured = True, status="Published").order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False, status="Published").order_by('-updated_at')
 
     context = {
-        "categories" : categories, 
         "featured_posts" : featured_posts,
         "posts" : posts,
     }
