b64e426ba2d26de92d1305aa5493dc6f064a9586
Shivam Bansal <shivambnsal@gmail.com>
2024-04-12 23:56:45 +0530

    Dashboard Users CRUD functionality for managers

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,12 +3,22 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+
+    # Categories CRUD
     path('categories/', views.categories, name='categories'),
     path('categories/add', views.add_category, name='add_category'),
     path('categories/edit/<int:pk>', views.edit_category, name='edit_category'),
     path('categories/delete/<int:pk>', views.delete_category, name='delete_category'),
+
+    # Blogs CRUD
     path('posts/', views.post, name='posts'),
     path('posts/add', views.add_post, name='add_post'),
     path('posts/edit/<int:pk>', views.edit_post, name='edit_post'),
     path('posts/delete/<int:pk>', views.delete_post, name='delete_post'),
+
+    # Users CRUD
+    path('users/', views.users, name='users'),
+    path('users/add/', views.add_user, name="add_user"),
+    path('users/edit/<int:pk>', views.edit_user, name="edit_user"),
+    path('users/delete/<int:pk>', views.delete_user, name='delete_user'),
 ]
